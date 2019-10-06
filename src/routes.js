6a9d3284848e6f324b764d792a35e61dc0bada7e
Cassio C <lccassiol@gmail.com>
2019-10-06 12:22:56 -0300

    Organize routes and add middleware after create user

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,26 @@
 import express from 'express';
 
+// Middlewares
+import authMiddleware from './app/middlewares/auth';
+
+// Controllers
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = express.Router();
 
 // Root route
 routes.get('/', (req, res) => res.json({ Api_status: 'working' }));
 
+// Session Controller
+routes.post('/session', SessionController.store);
+
 // User Routes
 routes.post('/users', UserController.store);
+
+// Apply Middlware
+routes.use(authMiddleware);
+
 routes.get('/users', UserController.index);
 routes.get('/users/:id', UserController.show);
 routes.put('/users', UserController.update);
